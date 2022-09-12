c9655457f2a0a88b240a2c1c19ed1f943fd9ea70
Zion Chen <zccmczh@gmail.com>
2022-09-12 11:21:04 +0000

    update

--- packages/magie/src/server/index.ts
+++ packages/magie/src/server/index.ts
@@ -1,6 +1,7 @@
 import { createServer as createHttpServer } from "http";
 import MagieConfig from "../types/MagieConfig";
+import { createServer as createViteServer } from 'vite'
 
 export function createDevServer(config: MagieConfig) {
-
+    
 }
