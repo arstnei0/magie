834fa99e455e3ecde8e5f9ef01e45ed04b7174d2
Tiis <zccmczh@gmail.com>
2022-09-20 12:16:15 +0000

    update

--- packages/test/magie.config.ts
+++ packages/test/magie.config.ts
@@ -3,4 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [vue()],
+    define: {
+        __LLL__: '"LLL"'
+    }
 });
--- packages/test/src/vite-env.d.ts
+++ packages/test/src/vite-env.d.ts
@@ -5,4 +5,5 @@ declare module '*.vue' {
     const component: DefineComponent<{}, {}, any>
     export default component
   }
-  
+
+declare const __LLL__: string
